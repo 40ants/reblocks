036a3e4748aa5e2b52233332d337bcfff902b66c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2019-04-14 11:28:26 +0300

    Another fix.

--- t/response.lisp
+++ t/response.lisp
@@ -44,13 +44,13 @@
   (with-session
     (with-request ("http://example.com:10050/foo/bar")
       (ok (equal (add-retpath-to "/login")
-                 "/login?retpath=http%3A%2F%2Fexample.com%3A10050%2Ffoo%2Fbar%3F")
+                 "/login?retpath=http%3A%2F%2Fexample.com%3A10050%2Ffoo%2Fbar")
           "Retpath should be added as a single parameter.")
       
       (ok (equal (add-retpath-to "/login?retpath=%2Fprojects")
-                 "/login?retpath=http%3A%2F%2Fexample.com%3A10050%2Ffoo%2Fbar%3F")
+                 "/login?retpath=http%3A%2F%2Fexample.com%3A10050%2Ffoo%2Fbar")
           "Existing retpath should be overwritten.")
       
       (ok (equal (add-retpath-to "/login?code=100500&retpath=%2Fprojects")
-                 "/login?code=100500&retpath=http%3A%2F%2Fexample.com%3A10050%2Ffoo%2Fbar%3F")
+                 "/login?code=100500&retpath=http%3A%2F%2Fexample.com%3A10050%2Ffoo%2Fbar")
           "Existing parameters should be leaved as is."))))
